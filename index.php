7659f94b9b6bd88c78912010133087766055c848
Ramirez-Dayana <93233944+8443-daramirez7@users.noreply.github.com>
2023-05-15 19:13:30 -0600

    prueba

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+	<title>Matriculas Vehículos - PARTE II</title>
+	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+	    <link rel="stylesheet" href="Recursos/estilos.css" type="text/css"/>
+</head>
+<body>
+	<div id="header" class="">
+            <img id="Espe_img" class="" src="Recursos/img/logo_ESPE.png" style="position:absolute;left:350px;top:40px;width:500px;height:89px;">
+            
+        </div>
+
+         <div id="navBar" class="Texto">
+        <table align="center" border="0">
+          <tr>
+             <td> <a href="vehiculo.php">Vehículo</a> </td>
+             <td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td> 
+             <td> <a href="marca.php">Marcas</a> </td> 
+             <td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </td> 
+             <td> <a href="agencia.php">Agencia</a> </td> 
+          </tr>
+        </table>
+      </div>
+
+      <!-- leftMenu (Rectangle) -->
+         <div id="leftMenu" class="">
+            <a href="" class="Boton">Matricular</a>
+            <a href="" class="Boton">Buscar</a>
+                 
+         </div>
+
+
+      <!-- content (Rectangle) -->
+         <div id="content" class="Texto" data-label="content">
+              <h2>DESARROLLO WEB PARA INTEGRACION</h2>
+              
+              <p>Estudiante: Ramirez Dayana</p>
+              <p>NRC: 10050</p>
+              <p>Fecha: 15/05/2023</p>
+             
+         </div>
+
+      <!-- rightMenu (Rectangle) -->
+         <div id="rightMenu" class="">
+           <iframe width="390" height="300" src="https://www.youtube.com/embed/siRLSq6BY0s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> 
+         </div>
+
+
+      <!-- footer (Rectangle) -->
+         <div id="footer" class="Texto">
+        
+             <a href="FormContacto.html">Contacto</a>
+
+         </div>
+
+</body>
+</html>
